6dfc44586cf77bc6a2a5304ff6c1921a6c704e0f
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2025-09-14 15:14:02 +0900

    Add debug command to generate prompt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { startServer } from "./server";
+import { shotPrompt } from "./server/services/prompts";
 
-startServer();
+(async () => {
+  if (process.argv.length > 2) {
+    const arg = process.argv[2];
+    await shotPrompt(arg);
+  } else {
+    startServer();
+  }
+})();
